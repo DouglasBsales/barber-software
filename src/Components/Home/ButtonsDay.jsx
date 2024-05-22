5da71d9ac32817c31e3aaa118fbaa0ba4628d2ac
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-22 15:19:29 -0300

    feature: change button inicialDay, finallyDay

--- src/Components/Home/ButtonsDay.jsx
+++ src/Components/Home/ButtonsDay.jsx
@@ -1,10 +1,27 @@
+import { HomeContext } from "@/Context/HomeContext";
+import { useContext } from "react";
+
 const ButtonsDay = () => {
+  const { initialDay, setInitialDay } = useContext(HomeContext);
+
   return (
     <div className="w-full flex justify-center pt-5 pb-6">
       <div>
-        <button className="bg-greenPrimary rounded-md">
-          <p className="py-3 px-16 text-white font-bold">Inicia o dia</p>
-        </button>
+        {initialDay ? (
+          <button
+            className="bg-greenPrimary rounded-md"
+            onClick={() => setInitialDay(false)}
+          >
+            <p className="py-3 px-16 text-white font-bold">Iniciar o dia</p>
+          </button>
+        ) : (
+          <button
+            className="bg-redPrimary rounded-md"
+            onClick={() => setInitialDay(true)}
+          >
+            <p className="py-3 px-16 text-white font-bold">Finalizar o dia</p>
+          </button>
+        )}
       </div>
     </div>
   );
