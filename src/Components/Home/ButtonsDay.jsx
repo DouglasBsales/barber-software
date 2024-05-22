8c1f1a759c941d5d4f12c78a314fd39f9642aefc
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-22 16:56:39 -0300

    feature: added function with alarme initialDay

--- src/Components/Home/ButtonsDay.jsx
+++ src/Components/Home/ButtonsDay.jsx
@@ -4,24 +4,22 @@ import { useContext } from "react";
 const ButtonsDay = () => {
   const { initialDay, setInitialDay } = useContext(HomeContext);
 
+  const initDay = ()=>{
+    alert("Dia iniciado!")
+    setInitialDay(false)
+  }
+
   return (
     <div className="w-full flex justify-center pt-5 pb-6">
       <div>
         {initialDay ? (
-          <button
-            className="bg-greenPrimary rounded-md"
-            onClick={() => setInitialDay(false)}
-          >
+          <button className="bg-greenPrimary rounded-md" onClick={initDay}>
             <p className="py-3 px-16 text-white font-bold">Iniciar o dia</p>
           </button>
-        ) : (
-          <button
-            className="bg-redPrimary rounded-md"
-            onClick={() => setInitialDay(true)}
-          >
-            <p className="py-3 px-16 text-white font-bold">Finalizar o dia</p>
-          </button>
-        )}
+        ):  <button className="bg-redPrimary rounded-md" onClick={()=> setInitialDay(true)}>
+        <p className="py-3 px-16 text-white font-bold">Finalizar o dia</p>
+      </button>}
+       
       </div>
     </div>
   );
