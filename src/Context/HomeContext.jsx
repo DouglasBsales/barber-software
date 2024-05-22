4fcbe42ad0cedff27795ddd03d3453f53cd2be69
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-22 15:20:23 -0300

    feature: added new consts

--- src/Context/HomeContext.jsx
+++ src/Context/HomeContext.jsx
@@ -6,10 +6,28 @@ export const HomeContext = createContext();
 
 export default function HomeContextProvider({ children }) {
   const [seeValue, setSeeValue] = useState(true);
+  const [initialDay, setInitialDay] = useState(true);
   const [page, setPage] = useState("Home");
+  const [modalService, setModalService] = useState(false);
+
+  const [arrayValueTotal, setArrayValueTotal] = useState([]);
+  const [arrayValueDay, setAarrayValueDay] = useState([]);
 
   return (
-    <HomeContext.Provider value={{ seeValue, setSeeValue, page, setPage }}>
+    <HomeContext.Provider
+      value={{
+        seeValue,
+        setSeeValue,
+        initialDay,
+        setInitialDay,
+        page,
+        setPage,
+        modalService,
+        setModalService,
+        arrayValueTotal,
+        arrayValueDay,
+      }}
+    >
       {children}
     </HomeContext.Provider>
   );
