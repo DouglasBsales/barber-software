c96b8c44cf0732afbf124e0b2382237751e29ba5
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-22 13:56:18 -0300

    refactor: change local HomeContext

--- src/Context/HomeContext.jsx
+++ src/Context/HomeContext.jsx
@@ -6,9 +6,10 @@ export const HomeContext = createContext();
 
 export default function HomeContextProvider({ children }) {
   const [seeValue, setSeeValue] = useState(true);
+  const [page, setPage] = useState("Home");
 
   return (
-    <HomeContext.Provider value={{ seeValue, setSeeValue }}>
+    <HomeContext.Provider value={{ seeValue, setSeeValue, page, setPage }}>
       {children}
     </HomeContext.Provider>
   );
