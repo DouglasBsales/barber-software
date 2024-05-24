37320941e157974d45c5b7d502c6cb4bb7d6c2c1
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-24 13:54:25 -0300

    feature: changed function for using localStorage set items

--- src/Components/Home/ButtonsDay.jsx
+++ src/Components/Home/ButtonsDay.jsx
@@ -1,17 +1,51 @@
 import { HomeContext } from "@/Context/HomeContext";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 
 const ButtonsDay = () => {
-  const { initialDay, setInitialDay } = useContext(HomeContext);
+  const { initialDay, setInitialDay, arrayRelatorioDia, setArrayRelatorioDia } = useContext(HomeContext);
+  const [isClient, setIsClient] = useState(false);
 
-  const initDay = ()=>{
-    alert("Dia iniciado!")
-    setInitialDay(false)
-  }
+  useEffect(() => {
+    // Marcar que estamos no cliente
+    setIsClient(true);
 
-  const finishDay = ()=> {
-    alert("Dia finalizado!")
-    setInitialDay(true)
+    // Carregar o estado inicial do localStorage quando o componente é montado
+    const storedInitialDay = localStorage.getItem("initialDay");
+    if (storedInitialDay !== null) {
+      setInitialDay(JSON.parse(storedInitialDay));
+    }
+  }, [setInitialDay]);
+
+  const initDay = () => {
+    const hour = new Date().toLocaleTimeString("pt-BR");
+    const date = new Date().toLocaleDateString("pt-BR");
+    alert("Dia iniciado!");
+    setInitialDay(false);
+    localStorage.setItem("initialDay", JSON.stringify(false));
+    console.log(hour, date);
+  };
+
+  const finishDay = () => {
+    alert("Dia finalizado!");
+    setInitialDay(true);
+    
+    localStorage.setItem("initialDay", JSON.stringify(true));
+
+    setArrayRelatorioDia([]);
+    localStorage.setItem("Servicos_dia", JSON.stringify([]));
+    console.log(arrayRelatorioDia)
+  };
+
+  if (!isClient) {
+    return (
+      <div className="w-full flex justify-center pt-5 pb-6">
+        <div>
+          <button className="bg-gray-400 rounded-md">
+            <p className="py-3 px-16 text-white font-bold">Carregando...</p>
+          </button>
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -21,10 +55,11 @@ const ButtonsDay = () => {
           <button className="bg-greenPrimary rounded-md" onClick={initDay}>
             <p className="py-3 px-16 text-white font-bold">Iniciar o dia</p>
           </button>
-        ):  <button className="bg-redPrimary rounded-md" onClick={finishDay}>
-        <p className="py-3 px-16 text-white font-bold">Finalizar o dia</p>
-      </button>}
-       
+        ) : (
+          <button className="bg-redPrimary rounded-md" onClick={finishDay}>
+            <p className="py-3 px-16 text-white font-bold">Finalizar o dia</p>
+          </button>
+        )}
       </div>
     </div>
   );
