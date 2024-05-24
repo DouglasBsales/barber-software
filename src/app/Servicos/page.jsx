8aa41954cf631160cfe34e9e8fb7c0760d866328
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-24 13:54:02 -0300

    feature: added useEffect for using localStorage

--- src/app/Servicos/page.jsx
+++ src/app/Servicos/page.jsx
@@ -2,8 +2,27 @@
 
 import Header from "@/Components/Servicos/Header";
 import ServiceItens from "@/Components/Servicos/ServiceItens";
+import { ConfigContext } from "@/Context/ConfigContext";
+import { useContext, useEffect } from "react";
 
 export default function Servicos() {
+
+const {arrayAddedService, setArrayAddedService} = useContext(ConfigContext)
+  
+  useEffect (() => {
+    const storageItem = localStorage.getItem("Servicos_adicionados");
+    if (storageItem) {
+      setArrayAddedService(JSON.parse(storageItem));
+    }
+  }, [setArrayAddedService]);
+
+  useEffect(() => {
+    localStorage.setItem(
+      "Servicos_adicionados",
+      JSON.stringify(arrayAddedService)
+    );
+  }, [arrayAddedService]);
+
   return (
     <div className="w-full">
       <Header />
